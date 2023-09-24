c9c12750487bae599be7ba933935ce2cae5a466c
Jamie Coombes <coombes.jd@gmail.com>
2023-09-24 18:14:26 +0100

    init ✨

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { auth } from '../lib/firebase';
+    import { userStore } from 'sveltefire';
+
+    // import * from '$lib/firebase';
+
+    let user = userStore(auth);
+    console.log(user);
+    user.subscribe(userStore(auth));
+    // let user = 5;
+</script>
+
+
+<!-- {# if user}
+<h1>Welcome to SvelteKit with SvelteFire {$user?.displayName}</h1>
+{:else if } -->
+
+{user}
+
+{#if $user}
+    <p>Hi {$user.uid}</p>
+{:else}
+    <p>Sign in...</p>
+{/if}
+
+<p>Noot?</p>
