622c6714f0087801d8998ff4969df15f89004ab3
Jamie Coombes <coombes.jd@gmail.com>
2023-10-03 17:04:20 +0100

    EOD: beginning the game routes, next step is a game/new route

---
+++ src/routes/games/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+	import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+	import { firestore } from '$lib/firebase';
+    import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+    import { SignedIn, docStore, Doc, Collection, userStore } from "sveltefire";
+</script>
+
+<SignedIn let:auth let:user>
+    <Doc ref={'games/' + $page.params.id} let:data>
+        <h1>{user.email}</h1>
+        <h1>{data.text}</h1>
+    </Doc>
+</SignedIn>
